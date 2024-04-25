90507bed1dfe95bc594006879cb424049559f0de
ItaloGianfrancoSalasJulian <italo.salas@ennova-research.com>
2024-04-25 15:14:52 +0200

    feat: update to angular standalone

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,11 +1,14 @@
 
 import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { NgFor } from '@angular/common';
 
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css'],
+    standalone: true,
+    imports: [NgFor]
 })
 export class HomeComponent implements OnInit {
 
